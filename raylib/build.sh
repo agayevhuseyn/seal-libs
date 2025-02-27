057d736eabb8ff74835ae7ba9a240e997645627d
agayevhuseyn <huseynaga269@gmail.com>
2025-02-27 19:05:57 +0400

    5.0 version of raylib

--- raylib/build.sh
+++ raylib/build.sh
@@ -2,6 +2,7 @@
 
 SEAL_DIR="../src/"
 LIBNAME="raylib"
-RAYLIB_DIR="winraylib-src"
-gcc -fPIC -shared -lm -o "$LIBNAME.so" "$LIBNAME.c" "$SEAL_DIR/libdef.c" "$SEAL_DIR/ast.c" ./libraylib.a -I$SEAL_DIR
-x86_64-w64-mingw32-gcc -shared -lm -o "$LIBNAME.dll" "$LIBNAME.c" "$SEAL_DIR/libdef.c" "$SEAL_DIR/ast.c" -I$SEAL_DIR -lraylib -lgdi32 -lwinmm -L $RAYLIB_DIR/lib -I $RAYLIB_DIR/include
+VER="-5.0"
+RAYLIB_DIR="winraylib-src$VER"
+gcc -fPIC -shared -o "$LIBNAME.so" "$LIBNAME.c" "$SEAL_DIR/libdef.c" "$SEAL_DIR/ast.c" ./libraylib.a -lm -I$SEAL_DIR
+x86_64-w64-mingw32-gcc -shared -o "$LIBNAME.dll" "$LIBNAME.c" "$SEAL_DIR/libdef.c" "$SEAL_DIR/ast.c" -I$SEAL_DIR -lm -lraylib -lgdi32 -lwinmm -L $RAYLIB_DIR/lib -I $RAYLIB_DIR/include
