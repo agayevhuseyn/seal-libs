4fdc25b1de46ab10bf125e10ed7dd4dbdf642ec2
agayevhuseyn <huseynaga269@gmail.com>
2025-03-14 08:33:24 +0400

    random library added

---
+++ random/random.c
@@ -0,0 +1,17 @@
+#include "../src/libdef.h"
+#include <stdlib.h>
+#include <time.h>
+
+static const char* libname = "random";
+
+void _initlib() {
+  srand(time(0));
+}
+
+sealobj* _rand(sealobj** args, size_t arg_size)
+{
+  seal_check_args(libname, "rand", NULL, 0, args, arg_size);
+  sealobj* res = create_ast(AST_INT);
+  res->integer.val = rand();
+  return res;
+}
